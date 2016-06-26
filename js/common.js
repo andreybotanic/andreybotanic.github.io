97e212959e0f5179eb853de5a8c802c0bc35765d
Yanduganov Andrey <andreybotanic@mail.ru>
2016-06-26 18:53:23 +0600

    Завершен раздел "Дисциплины"

--- js/common.js
+++ js/common.js
@@ -5,7 +5,11 @@ $(document).ready(function(){
         $(".tab").filter(function(){
             return this.id.match("profilesTab*");
         }).removeClass("tab_active");
+         $(".tab").filter(function(){
+            return this.id.match("profilesTab*");
+        }).addClass("tab_inactive");
         $($(this).attr("href")).addClass("tab_active");
+        $($(this).attr("href")).removeClass("tab_inactive");
     });
     
     $(".study").click(function(){
@@ -14,15 +18,23 @@ $(document).ready(function(){
         $(".tab").filter(function(){
             return this.id.match("studyTab*");
         }).removeClass("tab_active");
+         $(".tab").filter(function(){
+            return this.id.match("studyTab*");
+        }).addClass("tab_inactive");
         $($(this).attr("href")).addClass("tab_active");
+        $($(this).attr("href")).removeClass("tab_inactive");
     });
     
-     $(".subjects").click(function(){
+    $(".subjects").click(function(){
         $(".subjects").removeClass("tabpanel__link_active");
         $(this).addClass("tabpanel__link_active");
         $(".tab").filter(function(){
             return this.id.match("subjectsTab*");
         }).removeClass("tab_active");
+        $(".tab").filter(function(){
+            return this.id.match("subjectsTab*");
+        }).addClass("tab_inactive");
         $($(this).attr("href")).addClass("tab_active");
+        $($(this).attr("href")).removeClass("tab_inactive");
     });
 });
